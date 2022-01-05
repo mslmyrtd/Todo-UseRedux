3a53774da174592007de953b5ef90386e8d6b873
mslmyrtd <mslmyrtd@gmail.com>
2022-01-05 17:34:49 +0300

    dispatch added todoinput

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -1,12 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../redux/actions/todoAction";
 const TodoInput = () => {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
   const handleSubmit = (e) => {
+    if (text) {
+      dispatch(addTodo(text));
+      setText("");
+    }
     e.preventDefault();
   };
   return (
     <form onSubmit={handleSubmit}>
-      <input className="todo-input" type="text" placeholder="Add Todo" />
+      <input
+        className="todo-input"
+        type="text"
+        placeholder="Add Todo"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
       <button className="add-button" onClick={handleSubmit}>
         Add
       </button>
