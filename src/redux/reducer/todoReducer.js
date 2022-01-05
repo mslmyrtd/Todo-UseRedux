3f2c0efdebb47d71c8ae58e8a61d2490abe85809
mslmyrtd <mslmyrtd@gmail.com>
2022-01-05 17:23:59 +0300

    reducer

--- src/redux/reducer/todoReducer.js
+++ src/redux/reducer/todoReducer.js
@@ -1,7 +1,54 @@
+import {
+  ADD_TODO,
+  CLEAR_TODO_LIST,
+  DELETE_TODO,
+  TOGGLE_TODO,
+} from "../types/todoType";
+
 const initialState = {
-  count: 0,
+  counter: 0,
+  list: [
+    {
+      id: 0,
+      text: "add task",
+      completed: false,
+    },
+  ],
 };
 
-const todoReducer = (state = initialState, action) => {};
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        counter: state.counter + 1,
+        list: [
+          ...state.list,
+          {
+            id: state.counter + 1,
+            text: action.payload,
+            completed: false,
+          },
+        ],
+      };
+    case DELETE_TODO:
+      return {
+        ...state,
+        list: state.list.filter((item) => item.id !== action.payload),
+      };
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        list: state.list.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    case CLEAR_TODO_LIST:
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 export default todoReducer;
